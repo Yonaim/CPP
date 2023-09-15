d0f9bdb253beaa1aa9297ceb25f40557ecdd2500
yonaCluster <yonaimyo@gmail.com>
2023-09-15 15:41:18 +0900

    Refactor: formatting

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -8,66 +8,64 @@ Bureaucrat::Bureaucrat() : _name(default_name), _grade(default_grade)
 
 Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name), _grade(grade)
 {
-	if (_grade < highest_grade)
-		throw (GradeTooHighException());
-	if (_grade > lowest_grade)
-		throw (GradeTooLowException());
+    if (_grade < highest_grade)
+        throw(GradeTooHighException());
+    if (_grade > lowest_grade)
+        throw(GradeTooLowException());
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &orig) : _name(orig.getName()), _grade(orig.getGrade())
 {
-	if (_grade < highest_grade)
-		throw (GradeTooHighException());
-	if (_grade > lowest_grade)
-		throw (GradeTooLowException());
+    if (_grade < highest_grade)
+        throw(GradeTooHighException());
+    if (_grade > lowest_grade)
+        throw(GradeTooLowException());
 }
 
-Bureaucrat& Bureaucrat::operator=(const Bureaucrat &orig)
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &orig)
 {
-	if (this != &orig)
-	{
-		_name = orig.getName();
-		_grade = orig.getGrade();
-	}
-	return (*this);
+    if (this != &orig)
+    {
+        _name = orig.getName();
+        _grade = orig.getGrade();
+    }
+    return (*this);
 }
 
 Bureaucrat::~Bureaucrat()
 {
 }
 
-std::string	Bureaucrat::getName(void) const
+std::string Bureaucrat::getName(void) const
 {
-	return (_name);
+    return (_name);
 }
 
-
-int			Bureaucrat::getGrade(void) const
+int Bureaucrat::getGrade(void) const
 {
-	return (_grade);
+    return (_grade);
 }
 
-void		Bureaucrat::incrementGrade(void)
+void Bureaucrat::incrementGrade(void)
 {
-	if (_grade - 1 < highest_grade)
-		throw (GradeTooHighException());
-	_grade--;
+    if (_grade - 1 < highest_grade)
+        throw(GradeTooHighException());
+    _grade--;
 }
 
-void		Bureaucrat::decrementGrade(void)
+void Bureaucrat::decrementGrade(void)
 {
-	if (_grade + 1 < lowest_grade)
-		throw (GradeTooLowException());
-	_grade++;
+    if (_grade + 1 < lowest_grade)
+        throw(GradeTooLowException());
+    _grade++;
 }
 
-const char*	Bureaucrat::GradeTooHighException::what(void) const throw()
+const char *Bureaucrat::GradeTooHighException::what(void) const throw()
 {
-	return ("Bureaucrat: GradeTooHighException");
+    return ("Bureaucrat: GradeTooHighException");
 }
 
-
-const char*	Bureaucrat::GradeTooLowException::what(void) const throw()
+const char *Bureaucrat::GradeTooLowException::what(void) const throw()
 {
-	return ("Bureaucrat: GradeTooLowException");
+    return ("Bureaucrat: GradeTooLowException");
 }
--- CPP05/ex01/Form.cpp
+++ CPP05/ex01/Form.cpp
@@ -1,78 +1,73 @@
 #include "Form.hpp"
 #include "Bureaucrat.hpp"
 
-Form::Form()
-: _name("default"), _grade_to_sign(100), _grade_to_exec(100)
+Form::Form() : _name("default"), _grade_to_sign(100), _grade_to_exec(100)
 {
-
 }
 
 Form::Form(const std::string &name, int grade_to_sign, int grade_to_exec)
-: _name(name), _grade_to_sign(grade_to_sign), _grade_to_exec(grade_to_exec)
+    : _name(name), _grade_to_sign(grade_to_sign), _grade_to_exec(grade_to_exec)
 {
 }
 
 Form::Form(const Form &orig)
-: _name(orig.getName()),
-_is_signed(orig.getIsSigned()),
-_grade_to_sign(orig.getGradeRequiredToSign()),
-_grade_to_exec(orig.getGradeRequiredToExecute())
+    : _name(orig.getName()), _is_signed(orig.getIsSigned()), _grade_to_sign(orig.getGradeRequiredToSign()),
+      _grade_to_exec(orig.getGradeRequiredToExecute())
 {
 }
 
-Form& Form::operator=(const Form &orig)
+Form &Form::operator=(const Form &orig)
 {
-	if (this != &orig)
-	{
-		// 상수성을 제거하기 위해 const_cast 사용
-		// (const_cast는 포인터와 레퍼런스에만 사용 가능)
-		*(const_cast<std::string*>(&_name)) = orig.getName();		// _name = 
-		_is_signed = orig.getIsSigned();
-		*(const_cast<int*>(&_grade_to_sign)) = orig.getGradeRequiredToSign();
- 		*(const_cast<int*>(&_grade_to_exec)) = orig.getGradeRequiredToExecute();
-	}
-	return (*this);
+    if (this != &orig)
+    {
+        // 상수성을 제거하기 위해 const_cast 사용
+        // (const_cast는 포인터와 레퍼런스에만 사용 가능)
+        *(const_cast<std::string *>(&_name)) = orig.getName();
+        _is_signed = orig.getIsSigned();
+        *(const_cast<int *>(&_grade_to_sign)) = orig.getGradeRequiredToSign();
+        *(const_cast<int *>(&_grade_to_exec)) = orig.getGradeRequiredToExecute();
+    }
+    return (*this);
 }
 
 Form::~Form()
 {
 }
 
-
 std::string Form::getName(void) const
 {
-	return (_name);
+    return (_name);
 }
 
-bool	Form::getIsSigned(void) const
+bool Form::getIsSigned(void) const
 {
-	return (_is_signed);
+    return (_is_signed);
 }
 
-int	Form::getGradeRequiredToSign(void) const
+int Form::getGradeRequiredToSign(void) const
 {
-	return (_grade_to_sign);
+    return (_grade_to_sign);
 }
 
-int	Form::getGradeRequiredToExecute(void) const
+int Form::getGradeRequiredToExecute(void) const
 {
-	return (_grade_to_exec);
+    return (_grade_to_exec);
 }
 
-void	Form::beSigned(const Bureaucrat &bure)
+void Form::beSigned(const Bureaucrat &bure)
 {
-	if (_grade_to_sign > bure.getGrade())
-		throw (GradeTooLowException());
-	else
-		_is_signed = true;
+    if (_grade_to_sign > bure.getGrade())
+        throw(GradeTooLowException());
+    else
+        _is_signed = true;
 }
 
-const char*	Form::GradeTooHighException::what(void) const throw()
+const char *Form::GradeTooHighException::what(void) const throw()
 {
-	return ("Form: GradeTooHighException");
+    return ("Form: GradeTooHighException");
 }
 
-const char*	Form::GradeTooLowException::what(void) const throw()
+const char *Form::GradeTooLowException::what(void) const throw()
 {
-	return ("Form: GradeTooLowException");
+    return ("Form: GradeTooLowException");
 }
